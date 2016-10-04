917a7d34757f1d8ceb69b291cc569f161a1b6756
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2016-10-04 18:12:21 +0200

    Minor fixes after the dev workflow

--- generators/app/templates/default/scripts/setup-dev.sh
+++ generators/app/templates/default/scripts/setup-dev.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+set -e
 echo "NODE_ENV=development" >> .env
+
+if [ -n "$NPM_TOKEN" ]; then
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
+else
+  echo "NPM_TOKEN environment variable is not set. You may not be able to install private modules."
+  touch .npmrc
+fi
