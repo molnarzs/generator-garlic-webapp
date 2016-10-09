9a7bf2a3734bf0e9c82a761444605d3f374687a9
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2016-10-10 01:31:31 +0200

    semantic release fix

--- generators/angular-docker/templates/default/docker/semantic-release.sh
+++ generators/angular-docker/templates/default/docker/semantic-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker run \
   -v $(pwd):/app/project \
-  -e CI
-  -e TRAVIS
+  -e CI \
+  -e TRAVIS \
   -e TRAVIS_BRANCH \
   <%= c.dockerRepo %>/workflows-common npm run semantic-release
