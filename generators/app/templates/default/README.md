f3e4407f74383a006b986f9154837dac839e95c8
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2016-10-14 19:01:00 +0200

    Adding travis generator...

--- generators/app/templates/default/README.md
+++ generators/app/templates/default/README.md
@@ -6,3 +6,30 @@
 
 We use the [Docker based GarlicTech workflow manager](https://github.com/garlictech/workflows) to control development, build, deployment. 
 See the appropriate sections there, It is * important *. For the description of `npm run` commands, etc.
+
+Here, we summarize the mos important points.
+
+### Set up the development environment
+
+After cloning a repo:
+
+```
+npm run setup-dev
+```
+
+It creates the default `.env` file.
+
+
+### .env file
+
+It sets the following environment variables:
+
+* `NODE_ENV`
+
+Default: `development`. In this case, the `npm run unittest` command does not exit, it watches file changes.
+
+* `PORT`
+
+The port where the local server is mapped. The mapping is done in one of the docker-compose files. Defaulr: 8081. Mind, that inside the container, all the (dev) web servers
+are bound to port 8081, and docker compose maps this port to the value defined in `PORT`.
+
---
+++ generators/travis/index.js
@@ -0,0 +1,84 @@
+var GarlicWebappGithubGenerator, chalk, generatorLib, path, util, yeoman;
+
+util = require('util');
+
+path = require('path');
+
+yeoman = require('yeoman-generator');
+
+chalk = require('chalk');
+
+generatorLib = require('../lib');
+
+GarlicWebappGithubGenerator = yeoman.generators.Base.extend({
+  initializing: {
+    init: function() {
+      console.log(chalk.magenta('You\'re using the GarlicTech travis config generator.'));
+      return generatorLib.createConfig.bind(this)();
+    }
+  },
+  prompting: function() {
+    var cb, dockerPassword, dockerUser, done, slackToken;
+    done = this.async();
+    cb = (function(_this) {
+      return function(answers) {
+        _this.answers = answers;
+        return done();
+      };
+    })(this);
+    slackToken = process.env["SLACK_TOKEN_" + this.conf.scopeCC];
+    dockerUser = process.env.DOCKER_USER;
+    dockerPassword = process.env.DOCKER_PASSWORD;
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'slackToken',
+        "default": slackToken,
+        message: "Slack token: (we take the default from the environment variable SLACK_TOKEN_" + this.conf.scopeCC + "):",
+        store: true
+      }, {
+        type: 'input',
+        name: 'dockerUser',
+        "default": dockerUser,
+        message: "Docker private repo username: (we take the default from the environment variable DOCKER_USER):",
+        store: true
+      }, {
+        type: 'input',
+        name: 'dockerPassword',
+        "default": dockerPassword,
+        message: "Docker private repo password: (we take the default from the environment variable DOCKER_PASSWORD):",
+        store: true
+      }, {
+        type: 'input',
+        name: 'dockerMachine',
+        "default": "",
+        message: "Enter the SSH access of the docker machine this repo uses. Keep it empty if the project does not use docker docker machine. Example: root@api.gtrack.events",
+        store: true
+      }
+    ], cb.bind(this));
+  },
+  writing: {
+    createConfig: function() {
+      var done;
+      done = this.async();
+      this.conf.dockerUser = this.answers.dockerUser;
+      this.conf.dockerPassword = this.answers.dockerPassword;
+      this.conf.slackToken = this.answers.slackToken;
+      this.conf.dockerMachine = this.answers.dockerMachine;
+      return done();
+    },
+    mainFiles: function() {
+      return this.fs.copyTpl(this.templatePath('default/**/*'), this.destinationPath("./"), {
+        c: this.conf
+      });
+    },
+    executeScript: function() {
+      var done;
+      done = this.async();
+      generatorLib.execute(". ./travis_config.sh");
+      return done();
+    }
+  }
+});
+
+module.exports = GarlicWebappGithubGenerator;
