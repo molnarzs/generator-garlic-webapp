d53808fed2d0faa69a4072dc8c3b77c9cd466caa
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-01-25 22:22:56 +0100

    Fixing karma / webpack hooks

--- generators/angular2/templates/default/hooks/karma/karma.conf.js
+++ generators/angular2/templates/default/hooks/karma/karma.conf.js
@@ -1,6 +1,7 @@
 // The parameter is the prepared weback config, as the conteiner would use. There is no more configuration
 // after calling this function.
-module.exports = function(webpackConfig) {
+module.exports = function(karmaConfig) {
   // ===== yeoman hook config end =====
   // DO NOT REMOVE THE ABOVE LINE! Generators may not work.
+  console.log("**************3", karmaConfig);
 };
