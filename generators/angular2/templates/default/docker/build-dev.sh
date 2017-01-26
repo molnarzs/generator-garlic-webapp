83e311d2f59d723bebc28e4575806401850a4ce6
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-01-26 19:27:26 +0100

    Support angular package locking

--- generators/angular2/templates/default/docker/build-dev.sh
+++ generators/angular2/templates/default/docker/build-dev.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 docker-compose -f docker/docker-compose.webpack.yml build $@
+# /app/package_project.json is the package.json in this project, copied into the container.
+docker-compose -f docker/docker-compose.webpack.yml run <%= conf.webpackServerName %> scripts/cat-package-json.sh > package.json
