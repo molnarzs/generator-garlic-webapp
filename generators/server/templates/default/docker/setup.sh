03b4d2dc93b9d700734f278f55f88e17a056871a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-02-28 07:22:48 +0100

    fix: changes for workflow 26

--- generators/server/templates/default/docker/setup.sh
+++ generators/server/templates/default/docker/setup.sh
@@ -1,2 +1,12 @@
 #! /usr/bin/env bash
+
+set -e
+echo "Generating .env..."
 cp docker/sample-env .env
+
+DOCKER_RUN_CMD="docker-compose -f docker/docker-compose.yml run -T <%= c.scope %>.<%= c.appNameKC %>.dev"
+# /app/package_project.json is the package.json in this project, copied into the container.
+echo "Updating package.json..."
+${DOCKER_RUN_CMD} scripts/cat-package-json.sh > package.json
+echo "Updating tslint.json..."
+${DOCKER_RUN_CMD} cat tslint.json > tslint.json
