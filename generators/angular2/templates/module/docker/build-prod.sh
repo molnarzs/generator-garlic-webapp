56ee63e84dedff74cf0f17140f50cd852f62458a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-04-19 15:43:16 +0200

    fix: angular 2 module build fix

--- generators/angular2/templates/module/docker/build-prod.sh
+++ generators/angular2/templates/module/docker/build-prod.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 set -e
 docker/npm.sh build:prod
-
-docker run -i -t --rm \
-  -v $(pwd):/app/project \
-  <%= conf.dockerRepo %>/workflows-common:${npm_package_config_dockerWorkflowVersion} gulp css html
