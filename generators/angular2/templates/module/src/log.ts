2d5948d7cad592269660f73ba3a506eef6bdbb27
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-05-05 16:13:47 +0200

    fix: Angular 2 logger module fix

--- generators/angular2/templates/module/src/log.ts
+++ generators/angular2/templates/module/src/log.ts
@@ -1,13 +1,12 @@
-import { Log, Level } from 'ng2-logger/ng2-logger'
+import { Log, Level } from 'ng2-logger'
 import { Logger } from 'ng2-logger/src/logger'
 
-export const log = Log.create('@pioneer-wst/main-website')
+export const log = Log.create('@<%= conf.scope %>/<%= conf.appNameKC %>')
 log.color = "black"
 
 export function DebugLog(target: Object, key: string, descriptor: TypedPropertyDescriptor<any>) {
   return {
     value: function (...args: any[]) {
-      // let a = args.map(aa => JSON.stringify(aa)).join();
       let result = descriptor.value.apply(this, args);
       log.d(`Call: ${target.constructor.name}.${key}`, {arguments: args});
       return result;
